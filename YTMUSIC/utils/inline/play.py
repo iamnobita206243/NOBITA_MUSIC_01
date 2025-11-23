9b10bc6807e3fd2c665f2aeab42fa9160456a73b
iamnobita206243 <nobitamusic001@gmail.com>
2025-11-23 12:47:06 +0530

    Update play.py

--- YTMUSIC/utils/inline/play.py
+++ YTMUSIC/utils/inline/play.py
@@ -80,10 +80,6 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         url="https://t.me/II_UTKARSH_DEFAULTERS_II"
     ),
     InlineKeyboardButton(
-        text="Coder ❤️‍🔥", 
-        url="https://t.me/ll_NOBITA_DEFAULTERS_ll"
-    ),
-    InlineKeyboardButton(
         text="Support ❤️‍🔥", 
         url="https://t.me/+ClpXmI00B7UxYjc1"
     ),
