f9d031b14641427db0c5b40f66354de36ff1117e
iamnobita206243 <nobitamusic001@gmail.com>
2025-11-23 12:45:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ________________________________________________________________________________#
 API_URL = getenv("API_URL", 'https://pytdbotapi.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBotsb1880e')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots254c6b')
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
